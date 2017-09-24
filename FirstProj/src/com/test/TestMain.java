57d81228168ad4cc42054d996b5cd7f95a32b5c8
krishnaiah, avvaru <krishnaiah.avvaru@hotmail.com>
2017-09-24 10:19:55 -0400

    Test2

---
+++ FirstProj/src/com/test/TestMain.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.test;
+
+/**
+ * @author avaru
+ *
+ */
+public class TestMain {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ System.out.println("test");
+	}
+
+}
